12da78a832da0c45d045e3f335a326a4fd44e98f
alexander-svendsen <alexander.svendsen@nav.no>
2017-11-28 14:47:25 +0100

    Add method to log any json msg into ES

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ ADD / /source
 RUN build /source
 
 FROM docker.adeo.no:5000/bekkci/nais-deployer as deployer
-FROM docker.adeo.no:5000/bekkci/backend-smoketest as smoketest
 
 FROM docker.adeo.no:5000/bekkci/nais-java-app
 COPY --from=builder /source/target/frontendlogger /app
--- src/main/java/no/nav/LoggRessurs.java
+++ src/main/java/no/nav/LoggRessurs.java
@@ -1,16 +1,40 @@
 package no.nav;
 
+import org.slf4j.Logger;
+import org.slf4j.Marker;
+import org.slf4j.event.Level;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+import static net.logstash.logback.marker.Markers.appendEntries;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
-@Path("/")
+@Path("/{level}")
 public class LoggRessurs {
 
-    @GET
-    public String get() {
-        return "alt ok!";
+    private static final Logger LOG = getLogger(LoggRessurs.class);
+
+    private static final Map<Level, BiConsumer<Marker, String>> logMap;
+
+    static {
+        logMap = new HashMap<>();
+        logMap.put(Level.TRACE, LOG::trace);
+        logMap.put(Level.DEBUG, LOG::debug);
+        logMap.put(Level.INFO, LOG::info);
+        logMap.put(Level.WARN, LOG::warn);
+        logMap.put(Level.ERROR, LOG::error);
+    }
+
+    @POST
+    public void log(@PathParam("level") String level, Map<?, ?> logMsg) {
+        Level logLevel = Level.valueOf(level.toUpperCase());
+        logMap.get(logLevel).accept(appendEntries(logMsg), null);
     }
 }
