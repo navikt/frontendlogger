6fcc50a95b350eaebbfe18d4737fb5cd61d82ede
Nicklas Utgaard <nicklas.utgaard@nav.no>
2018-08-27 15:31:06 +0200

    [KAIZEN-0] Tar ibruk pinpoint
    pinpoint brukes for a gi bedre feilmeldinger i kibana.
    Det trengs en avhengighet fra npmjs, derfor er bygget endret
    til a bruke browserify/uglifyJS i samme slengen

---
+++ src/frontend/src/index.js
@@ -0,0 +1,58 @@
+const serializError = require('serialize-error');
+const api = require('./api');
+
+const apiUrl = '/frontendlogger/api/';
+const appname = window.frontendlogger.appname;
+const oldOnError = window.onerror;
+
+function report(level, data) {
+    data.url = window.location.href;
+    data.userAgent = window.navigator.userAgent;
+    data.appname = appname;
+
+    return api.post(`${apiUrl}${level}`, data);
+}
+
+function log(level, data) {
+    if (typeof data === 'string') {
+        data = { message: data };
+    }
+    report(level, data);
+}
+
+function reportEvent(name, fields, tags) {
+    report('event', { name, fields, tags });
+}
+
+window.onerror = function (message, url, line, column, error) {
+    const json = {
+        message: message,
+        jsFileUrl: url,
+        lineNumber: line,
+        column: column,
+        messageIndexed: message
+    };
+
+    if (error) {
+        const body = {
+            message,
+            url,
+            line,
+            column,
+            error: serializError(error)
+        };
+        api.post('/pinpoint/api/pinpoint', body)
+            .then((resp) => resp.json())
+            .then((resolved) => report('error', {...json, ...resolved}))
+            .catch(() => report('error', json))
+    } else {
+        report('error', json);
+    }
+    if (oldOnError) {
+        oldOnError.apply(this, arguments);
+    }
+};
+window.frontendlogger.info = function(data) { log('info', data); };
+window.frontendlogger.warn = function(data) { log('warn', data); };
+window.frontendlogger.error = function(data) { log('error', data); };
+window.frontendlogger.event = function(name, fields, tags) { reportEvent(name, fields, tags); };
