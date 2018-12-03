a264a60aeb082f89e0a210db5694a4bdb8fa9b0a
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2018-12-03 12:27:58 +0100

    KAIZEN-0 opprettet prometheus-metrikk for logging

--- src/main/java/no/nav/LoggRessurs.java
+++ src/main/java/no/nav/LoggRessurs.java
@@ -1,5 +1,7 @@
 package no.nav;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import no.nav.metrics.MetricsFactory;
 import org.slf4j.Logger;
 import org.slf4j.Marker;
 import org.slf4j.event.Level;
@@ -12,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
+import static java.util.Optional.ofNullable;
 import static net.logstash.logback.marker.Markers.appendEntries;
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -20,6 +23,8 @@ import static org.slf4j.LoggerFactory.getLogger;
 public class LoggRessurs {
 
     private static final Logger LOG = getLogger(LoggRessurs.class);
+    private static final MeterRegistry meterRegistry = MetricsFactory.getMeterRegistry();
+    private static final String APPLICATION_ATTRIBUTE_NAME = "appname";
 
     private static final Map<Level, BiConsumer<Marker, String>> logMap;
 
@@ -35,6 +40,16 @@ public class LoggRessurs {
     @POST
     public void log(@PathParam("level") String level, Map<String, Object> logMsg) {
         Level logLevel = Level.valueOf(level.toUpperCase());
+        String appname = ofNullable(logMsg.get(APPLICATION_ATTRIBUTE_NAME)).map(Object::toString).orElse("unknown");
+
+        meterRegistry.counter(
+                "frontend_logger",
+                "origin_app",
+                appname,
+                "level",
+                logLevel.name()
+        ).increment();
+
         logMap.get(logLevel).accept(appendEntries(logMsg), null);
     }
 }
