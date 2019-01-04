fed27a01ded13bc12585ec5b995a3428a355c4fa
matiasvinjevoll <matiasvinjevoll@gmail.com>
2019-01-04 13:37:58 +0100

    timer

--- src/main/java/no/nav/YtelseRessurs.java
+++ src/main/java/no/nav/YtelseRessurs.java
@@ -1,7 +1,8 @@
-    package no.nav;
+package no.nav;
 
 import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import no.nav.metrics.MetricsFactory;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Path("/performance")
@@ -18,11 +20,19 @@ public class YtelseRessurs {
     private static final DistributionSummary summary =
             DistributionSummary
                     .builder("page_load_time")
-                    .sla(50, 70, 90, 95)
+                    .publishPercentileHistogram()
+                    .register(meterRegistry);
+
+    private static final Timer timer =
+            Timer
+                    .builder("page_load_time_2")
+                    .publishPercentileHistogram()
                     .register(meterRegistry);
 
     @POST
     public void lagEvent(Map<String, Object> logMsg) {
-         summary.record(Math.random() * 100);
+
+        summary.record(Math.random() * 100);
+        timer.record(Math.round(Math.random() * 100), TimeUnit.MILLISECONDS);
     }
 }
