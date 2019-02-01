a659dc4384835b401c35e70577e2280a884d6894
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2019-02-01 10:29:18 +0100

    refaktorerte integrasjon mot pinpoint

--- src/main/java/no/nav/LoggRessurs.java
+++ src/main/java/no/nav/LoggRessurs.java
@@ -2,25 +2,21 @@ package no.nav;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import no.nav.metrics.MetricsFactory;
-import no.nav.sbl.rest.RestUtils;
 import org.slf4j.Logger;
 import org.slf4j.Marker;
 import org.slf4j.event.Level;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.InvocationCallback;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
 import static java.util.Optional.ofNullable;
-import static javax.ws.rs.client.Entity.json;
 import static net.logstash.logback.marker.Markers.appendEntries;
-import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
@@ -33,7 +29,6 @@ public class LoggRessurs {
 
     private static final Map<Level, BiConsumer<Marker, String>> logMap;
 
-    private static final Client client = RestUtils.createClient();
 
     static {
         logMap = new HashMap<>();
@@ -44,7 +39,13 @@ public class LoggRessurs {
         logMap.put(Level.ERROR, LOG::error);
     }
 
-    private final String pinpointApiUrl = getOptionalProperty("PINPOINT_BASE_URL").orElse("https://pinpoint.default.svc.nais.local") + "/pinpoint/api/pinpoint";
+
+    private final PinpointClient pinpointClient;
+
+    @Inject
+    public LoggRessurs(PinpointClient pinpointClient) {
+        this.pinpointClient = pinpointClient;
+    }
 
     @POST
     public void log(@PathParam("level") String level, Map<String, Object> logMsg) {
@@ -59,41 +60,20 @@ public class LoggRessurs {
                 logLevel.name()
         ).increment();
 
+        Object pinpoint = logMsg.get("pinpoint");
+        if (pinpoint != null) {
+            logMsg.remove("pinpoint");
+        }
+        
         logToLogback(logLevel, logMsg);
-        logToPinpoint(logLevel, logMsg);
+
+        if (pinpoint != null) {
+            pinpointClient.enrichErrorData(pinpoint, (enrichedError) -> logToLogback(logLevel, enrichedError));
+        }
     }
 
     private static void logToLogback(Level logLevel, Map<String, Object> logMsg) {
         logMap.get(logLevel).accept(appendEntries(logMsg), null);
     }
 
-    private void logToPinpoint(Level logLevel, Map<String, Object> logMsg) {
-        Object pinpoint = logMsg.get("pinpoint");
-        if (pinpoint != null) {
-            logMsg.remove("pinpoint");
-            client.target(pinpointApiUrl)
-                    .request()
-                    .async()
-                    .post(json(pinpoint), new PinpointCallback(logLevel));
-        }
-    }
-
-    private static class PinpointCallback implements InvocationCallback<Map<String, Object>> {
-        private final Level logLevel;
-
-        private PinpointCallback(Level logLevel) {
-            this.logLevel = logLevel;
-        }
-
-        @Override
-        public void completed(Map<String, Object> map) {
-            logToLogback(logLevel, map);
-        }
-
-        @Override
-        public void failed(Throwable throwable) {
-            LOG.error(throwable.getMessage(), throwable);
-        }
-
-    }
 }
