7f3ca0fc7ad0d830ccedcfc03fb52c88536987e5
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2019-02-13 15:24:31 +0100

    endret fra fetch() til xhr for å støtte ie

--- src/frontend/src/api.js
+++ src/frontend/src/api.js
@@ -1,12 +1,7 @@
-require('whatwg-fetch');
 
 export function post(path, data) {
-    return fetch(path, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-
-        body: JSON.stringify(data)
-    });
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', path);
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(JSON.stringify(data));
 }
