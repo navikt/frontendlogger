4e5551bb293389f43002218d9e7dd607de1d60d3
Nicklas Utgaard <nutgaard@gmail.com>
2020-08-25 08:46:50 +0200

    [KAIZEN-0] debugging cors-setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,17 @@
 import no.nav.ApplicationConfig;
 import no.nav.apiapp.ApiApp;
 
+import java.util.Optional;
+
+import static no.nav.sbl.util.EnvironmentUtils.getOptionalProperty;
+
 public class Main {
 
     public static void main(String... args) throws Exception {
+        Optional<String> optionalProperty = getOptionalProperty("cors.allowed.origins");
+        System.out.println("Found CORS " + optionalProperty.isPresent());
+        System.out.println("Found CORS " + optionalProperty.orElse("N/A"));
+
         ApiApp.runApp(ApplicationConfig.class, args);
     }
 
