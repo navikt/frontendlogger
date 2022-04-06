62b0ab1946e0a7b9b5cff70431549ff53f335810
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-04-06 10:17:10 +0200

    Slett utkommentert/gammelt CORS-oppsett

--- src/main/java/no/nav/frontendlogger/Main.java
+++ src/main/java/no/nav/frontendlogger/Main.java
@@ -6,23 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String... args) throws Exception {
+    public static void main(String... args) {
         SslUtils.setupTruststore();
-//        setupCors();
         SpringApplication.run(Main.class, args);
     }
-/*
-    private static void setupCors() {
-        setProperty(CORS_ALLOWED_HEADERS, ALLOWED_HEADERS, EnvironmentUtils.Type.PUBLIC);
-        getOptionalProperty("CORS_ALLOWED_ORIGINS")
-                .ifPresent((value) -> setProperty(CORS_ALLOWED_ORIGINS, value, EnvironmentUtils.Type.PUBLIC));
-    }
-
-    private static final String ALLOWED_HEADERS = String.join(",",
-            "Accept",
-            "Accept-language",
-            "Content-Language",
-            "Content-Type",
-            "Nav-Consumer-Id"
-    );*/
 }
