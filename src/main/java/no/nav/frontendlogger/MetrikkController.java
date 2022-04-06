72b0e2bca7da25c473d1913d0b3a593fe3a5ad46
matiasvinjevoll <matias.vinjevoll@nav.no>
2022-04-06 13:02:08 +0200

    Legg til enviromnent tag for events for bakoverkompabilitet

--- src/main/java/no/nav/frontendlogger/MetrikkController.java
+++ src/main/java/no/nav/frontendlogger/MetrikkController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static no.nav.common.utils.EnvironmentUtils.isProduction;
+import static no.nav.common.utils.EnvironmentUtils.resolveHostName;
+
 @RestController
 @RequestMapping("/api/event")
 public class MetrikkController {
@@ -25,6 +28,27 @@ public class MetrikkController {
         event.getFields().forEach(newEvent::addFieldToReport);
         event.getTags().forEach(newEvent::addTagToReport);
 
+        // Legger til environment for bakoverkompabilitet
+        event.getTags().putIfAbsent("environment", resolveEnvironmentTag());
+
         metricsClient.report(newEvent);
+    }
+
+    private String resolveEnvironmentTag() {
+        if (isProduction().orElse(false)) {
+            return "p";
+        }
+
+        String hostName = resolveHostName();
+
+        if (hostName.contains("q1")) {
+            return "q1";
+        }
+
+        if (hostName.contains("q0")) {
+            return "q0";
+        }
+
+        return "q";
     }
 }
